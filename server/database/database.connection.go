b88e9da682570af0baff3cc38950dbc504497dba
madhav <eragon.cube@outlook.com>
2025-04-12 21:08:21 +0530

    Connecting to database...

--- server/database/database.connection.go
+++ server/database/database.connection.go
@@ -1,1 +1,37 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"log"
+	"time"
+)
+
+func DbInstance() *mongo.Client {
+	mongoDB := "mongodb://localhost:27017"
+	fmt.Println(mongoDB)
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("Connected to MongoDB")
+	return client
+}
+
+var Client = DbInstance()
+
+func OpenCollection(client *mongo.Client, collectionName string) *mongo.Collection {
+	collection := client.Database("restaurant").Collection(collectionName)
+	return collection
+}
